a3c0082f50016f6c417d5bf6efb6985b7be9dee0
Hyunsik <hyunsik987@naver.com>
2026-04-01 19:17:43 +0900

    [Gold V] Title: 적록색약, Time: 128 ms, Memory: 16256 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -0,0 +1,58 @@
+import java.io.*;
+
+public class Main {
+    static char[][] map;
+    static boolean[][] visited;
+    static int N;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine().trim());
+        map = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        // 일반인
+        visited = new boolean[N][N];
+        int normal = 0;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (!visited[i][j]) {
+                    dfs(i, j, map[i][j]);
+                    normal++;
+                }
+
+        // 전처리: R을 전부 G로 바꿔버림 (R과 G를 동일하게 만들기)
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (map[i][j] == 'R') map[i][j] = 'G';
+
+        // 적록색약
+        visited = new boolean[N][N];
+        int blind = 0;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (!visited[i][j]) {
+                    dfs(i, j, map[i][j]);
+                    blind++;
+                }
+
+        System.out.println(normal + " " + blind);
+    }
+
+    static void dfs(int x, int y, char color) {
+        visited[x][y] = true;
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N
+                    && !visited[nx][ny] && map[nx][ny] == color) {
+                dfs(nx, ny, color);
+            }
+        }
+    }
+}
