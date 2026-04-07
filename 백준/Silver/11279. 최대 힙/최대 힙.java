7329bf7535a809bb90b2064781925f5722cc11f4
Hyunsik <hyunsik987@naver.com>
2026-04-08 08:44:36 +0900

    [Silver II] Title: 최대 힙, Time: 292 ms, Memory: 26292 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> maxQ = new PriorityQueue<>((a, b) -> b - a);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int type = Integer.parseInt(br.readLine());
+            if (type == 0) {
+                if (maxQ.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(maxQ.poll()).append("\n");
+                }
+            } else {
+                maxQ.offer(type);
+            }
+        }
+        System.out.println(sb);
+    }
+}
